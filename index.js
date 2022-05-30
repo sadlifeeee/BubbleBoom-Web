1debd64c84eec1485e938448af42baa113906199
Jerickson <jerickson_lee@dlsu.edu.ph>
2022-05-30 21:46:35 +0800

    Major Changes, Ready for Deployment

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const fs = require('fs');
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
+const favicon = require('serve-favicon');
+const path = require('path');
 
+
+app.use('/favicon.ico', express.static('public/img/favicon.ico'));
 app.set('view engine' , 'hbs');
 
 dotenv.config(); 
 hbs.registerPartials(__dirname + '/views/partials')
-
-
 app.use(express.static(__dirname + '/public'));
-
 
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
@@ -20,6 +21,13 @@ app.get('/' , function(req, res) {
     res.render('home.hbs');
 });
 
+app.get('/about' , function(req , res) {
+    res.render('about.hbs');
+});
+
+app.get('/products' , function(req , res) {
+    res.render('products.hbs');
+});
 
 app.listen(port , hostname , function(){
     console.log('Server Running at:');
